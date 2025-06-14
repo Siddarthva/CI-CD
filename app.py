7c967ae71060c65caeedc6753235701952675ecb
Siddarthva <sidarthvacharyaa@gmail.com>
2025-06-14 05:31:19 +0530

    Final update

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ def home():
     return "<h1>Hello from Flask app in Docker!</h1>"
 
 app.run(host='0.0.0.0', port=5000)
+
+
+
+#docker build -t flask-web-app .
+
+#docker run -d -p 8081:5000 --name flask-container flask-web-app
