761190c79d8e762952325faf91fb99fa71aa8ef8
Siddarthva <sidarthvacharyaa@gmail.com>
2025-06-14 05:50:32 +0530

    Final update

--- app.py
+++ app.py
@@ -12,3 +12,15 @@ app.run(host='0.0.0.0', port=5000)
 #docker build -t flask-web-app .
 
 #docker run -d -p 8081:5000 --name flask-container flask-web-app
+
+
+#For jenkins pipeline, use the following command:
+# Remove the existing Docker container named 'flask-container' if it exists
+#   docker rm --force flask-container || true
+
+# Build a new Docker image with the tag 'flask-web-app' from the current directory
+#   docker build -t flask-web-app .
+
+# Run a new Docker container from the image 'flask-web-app'
+# It will run in detached mode (-d), map host port 8081 to container port 5000 (Flask default), and name the container 'flask-container'
+#   docker run -d -p 8081:5000 --name=flask-container flask-web-app
